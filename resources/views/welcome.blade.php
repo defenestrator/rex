d61ca0238f164e7e3ef61e392afa06900ea3e847
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-06-25 17:12:01 -0600

    layout, login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -407,7 +407,7 @@
     <div
         class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <div class="fixed top-0 right-0 px-6 py-4 block">
                 @auth
                     <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                 @else
@@ -423,7 +423,7 @@
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                 <div class="h-16 w-auto text-gray-700 sm:h-20">
-                    <div style="color:#EF3B2D; font-size:3rem;">
+                    <div class="p-2 text-center" style="color:#EF3B2D; font-size:3rem;">
                         Reptile Experience Center
                     </div>
                 </div>
