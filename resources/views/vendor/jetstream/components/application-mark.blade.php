ff2a7d395286c2dff70566eb89515dbd8d2f4339
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-06-25 19:42:25 -0600

    saasdbabfs

--- resources/views/vendor/jetstream/components/application-mark.blade.php
+++ resources/views/vendor/jetstream/components/application-mark.blade.php
@@ -1,2 +1,2 @@
-<img style="width:60px;" class="py-4" src="favicon-100.png" alt="Intermountain Reptile Experience Center"
+<img class="py-4" src="favicon-100.png" alt="Intermountain Reptile Experience Center"
     title="Intermountain Reptile Experience Center">
