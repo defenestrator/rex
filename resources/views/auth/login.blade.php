b24f45448508992f9619db971ef335d93de48eba
Jeremy Anderson <529446+defenestrator@users.noreply.github.com>
2021-06-25 19:32:36 -0600

    register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,12 +17,14 @@
 
             <div>
                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required autofocus />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -31,10 +33,16 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
             <div class="flex items-center justify-end mt-4">
+                @if (Route::has('register'))
+                    <a class="px-4 flex-grow-0 underline text-sm text-gray-600 hover:text-gray-900"
+                        href="{{ route('register') }}">
+                        {{ __('Register?') }}
+                    </a>
+                @endif
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="px-4 underline text-sm text-gray-600 hover:text-gray-900"
+                        href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
